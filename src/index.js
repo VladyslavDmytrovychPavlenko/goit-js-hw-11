cd2b5945529ceb71f1fd7d1eeebaee444c7cf280
VladyslavDmytrovychPavlenko <onlyforonlineworkvlad@gmail.com>
2023-04-04 00:28:48 +0300

    fix mentor

--- src/index.js
+++ src/index.js
@@ -1,94 +1,75 @@
-// Імпорт необхідних модулів та таблиць стилів
 import './sass/index.scss';
 import { fetchPictures } from './js/pictureApiService';
 import { Notify } from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// Отримуєм елементи DOM і приєднуєм слухачі подій
 const searchForm = document.querySelector('.search-form');
 const galleryBox = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+loadMoreBtn.classList.add('is-hiden');
+console.log(loadMoreBtn);
 searchForm.addEventListener('submit', onSearch);
 loadMoreBtn.addEventListener('click', onloadMore);
 
-// Встановити початковий стан
-loadMoreBtn.classList.add('is-hidden');
 let query = '';
 let page = 1;
-let simpleLightBox;
 const perPage = 40;
 
-// Функція зворотного виклику слухача подій для подання форми пошуку
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
-  // Скинути номер сторінки та пошуковий запит
   page = 1;
   query = e.currentTarget.elements.searchQuery.value.trim();
-  // Очистити галерею та показати кнопку завантажити більше
+  console.log(query);
   galleryBox.innerHTML = '';
-  if (!query) {
-    // Показувати повідомлення про помилку, якщо пошуковий запит порожній
-    Notify.failure(
-      'The search string cannot be empty. Please specify your search query.'
-    );
-    return;
-  }
-  loadMoreBtn.classList.remove('is-hidden');
-  // Отримання зображень з API та обробка відповіді
-  fetchPictures(query, page, perPage)
-    .then(({ data }) => {
-      if (data.totalHits === 0) {
-        // Показувати сповіщення про помилку, якщо зображення не знайдено
-        Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      // Створення HTML-розмітки для галереї та ініціалізація SimpleLightbox
-      createMarkup(data.hits);
-      simpleLightBox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-      }).refresh();
-      // Показати сповіщення про успіх із кількістю знайдених зображень
-      Notify.success(`Hooray! We found ${data.totalHits} images.`);
-    })
-    .catch(error => console.log(error))
-    .finally(() => {
-      // Скидання форми пошуку
+
+  loadMoreBtn.classList.add('is-hiden');
+  try {
+    const { hits, totalHits } = await fetchPictures(query, page, perPage);
+    console.log(hits);
+    console.log(totalHits);
+    if (!query || totalHits === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMoreBtn.classList.add('is-hiden');
+      galleryBox.innerHTML = '';
+      return;
+    } else if (hits.length < 40) {
+      createMarkup(hits);
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      loadMoreBtn.classList.add('is-hiden');
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    } else if (page > totalHits / perPage) {
+      loadMoreBtn.classList.add('is-hiden');
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    } else {
+      createMarkup(hits);
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      loadMoreBtn.classList.remove('is-hiden');
       searchForm.reset();
-    });
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-// Функція зворотного виклику слухача подій для натискання кнопки завантаження більше
-function onloadMore() {
-  // Збільшити номер сторінки та знищити SimpleLightbox
+async function onloadMore() {
   page += 1;
-  simpleLightBox.destroy();
-  // Отримати більше зображень з API та обробити відповідь
-  fetchPictures(query, page, perPage)
-    .then(({ data }) => {
-      // Створіть розмітку HTML для нових зображень і оновіть SimpleLightbox
-      createMarkup(data.hits);
-      simpleLightBox = new SimpleLightbox('.gallery a', {
-        captionsData: 'alt',
-        captionPosition: 'bottom',
-        captionDelay: 250,
-      }).refresh();
-      // Приховати кнопку завантаження додаткових відомостей і показати сповіщення про помилку, якщо досягнуто кінця результатів пошуку
-      const totalPages = Math.ceil(data.totalHits / perPage);
-      if (page > totalPages) {
-        loadMoreBtn.classList.add('is-hidden');
-        Notify.failure(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    })
-    .catch(error => console.log(error));
+
+  try {
+    const { hits, totalHits } = await fetchPictures(query, page, perPage);
+
+    createMarkup(hits);
+
+    if (page > totalHits / perPage) {
+      loadMoreBtn.classList.add('is-hiden');
+      Notify.info("We're sorry, but you've reached the end of search results.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 function createMarkup(hits) {
-  console.log(`Rendering...`);
   const markup = hits
     .map(
       ({
--- src/js/pictureApiService.js
+++ src/js/pictureApiService.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const KEY = '34889854-1af62613b01e34ca3c220e7a9';
-async function fetchPictures(query, page, perPage) {
-  const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-  );
-  return response;
+
+export async function fetchPictures(query, page, perPage) {
+  try {
+    const response = await axios.get(
+      `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
+    );
+    console.log(response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
-export { fetchPictures };
+// export { fetchPictures };
